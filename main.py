f03834dbe5f090de0791c9758a013d9a1ae87916
jeevan <jeevanjnayak@gmail.com>
2021-07-04 11:02:20 +0530

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+operator = input("enter the operator (+ or - or * or / ): ")
+result = 0
+if operator == "+":
+    result = a + b
+elif operator == "-":
+    result = a - b
+elif operator == "*":
+    result = a * b
+elif operator == "/":
+    result = a / b
+else:
+    print("you entered a wrong operator.")
+print(f"result = {result}")
