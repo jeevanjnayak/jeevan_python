cf9466bec74730787c98fffae069c1cbe043ed36
jeevan <jeevanjnayak@gmail.com>
2021-07-06 17:57:39 +0530

    dictionary commit

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 a = int(input("enter the first number: "))
 b = int(input("enter the second number: "))
-operator = input("enter the operator (+ or - or * or / ): ")
-result = 0
-if operator == "+":
-    result = a + b
-elif operator == "-":
-    result = a - b
-elif operator == "*":
-    result = a * b
-elif operator == "/":
-    result = a / b
-else:
-    print("you entered a wrong operator.")
-print(f"result = {result}")
+
+result = {}
+
+addition = a + b
+
+subtraction = a - b
+
+multiplication = a * b
+
+result["sum"] = addition
+result["difference"] = subtraction
+result["product"] = multiplication
+print(result)
